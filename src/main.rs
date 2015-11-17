cdd7d94ce07cbae69d00cf0f5ed4c4e97a20d674
Kim Sevandersson <ksevandersson@gmail.com>
2015-11-17 02:43:21 +0100

    Test some UI stuff

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 extern crate winapi;
 extern crate xinput;
+extern crate user32;
 
 mod gamepads;
+
+use std::ffi::OsStr;
+use std::os::windows::ffi::OsStrExt;
+
+use winapi::{MB_OK};
+use user32::MessageBoxW;
 
 use gamepads::BatteryType;
 
@@ -13,5 +20,15 @@ fn main() {
                 .collect();
 
     println!("{:?}", info);
-}
 
+    let mut title: Vec<_> = 
+        OsStr::new("Hello").encode_wide()
+                           .chain(Some(0).into_iter())
+                           .collect();
+
+    title.push(0u16);
+
+    let ptr = title.as_mut_ptr();
+
+    unsafe { MessageBoxW(std::ptr::null_mut(), ptr, ptr, MB_OK); }
+}
