6ee9913d8d0cdf7fa6750dc1db670ab536e8eab6
Kim Sevandersson <ksevandersson@gmail.com>
2015-11-18 21:58:01 +0100

    Some minor changes

--- src/main.rs
+++ src/main.rs
@@ -19,16 +19,18 @@ fn main() {
                 .filter(|bat| bat.kind != BatteryType::Disconnected)
                 .collect();
 
-    println!("{:?}", info);
+    
 
-    let mut title: Vec<_> = 
-        OsStr::new("Hello").encode_wide()
-                           .chain(Some(0).into_iter())
-                           .collect();
+    let mut title = encode_wide("Hello");
 
-    title.push(0u16);
+    let m = format!("{:?}", info);
+    let mut content: Vec<_> = encode_wide(&m);
 
-    let ptr = title.as_mut_ptr();
+    unsafe { MessageBoxW(std::ptr::null_mut(), content.as_mut_ptr(), title.as_mut_ptr(), MB_OK); }
+}
 
-    unsafe { MessageBoxW(std::ptr::null_mut(), ptr, ptr, MB_OK); }
+fn encode_wide<'a>(input: &'a str) -> Vec<u16> {
+    OsStr::new(input).encode_wide()
+                     .chain(std::iter::once(0))
+                     .collect()
 }
